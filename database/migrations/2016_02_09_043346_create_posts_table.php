71e528a7f371605acb38cb3f6081093be8988434
Gian Inductivo <gian@dynamicobjx.com>
2016-02-09 12:45:51 +0800

    Added migration for posts
    Posts (blog entries) have as a minimum, a title, content, status, a
    post date and an author.

---
+++ database/migrations/2016_02_09_043346_create_posts_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreatePostsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('posts', function (Blueprint $table) {
+            $table->increments('id');
+            $table->string('post_title');
+            $table->longText('post_content');
+            $table->dateTime('post_date');
+            $table->string('post_status');
+            $table->index('post_status');
+            $table->integer('post_author')->unsigned();
+            $table->foreign('post_author')->references('id')->on('users');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::drop('posts');
+    }
+}
